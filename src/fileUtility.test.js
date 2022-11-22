76a677a028b774eef58f9ed15bff74ea63b09c93
gwarren3210 <gavrielewarren@gmail.com>
2022-11-22 15:38:54 -0500

    utility test readFrom

--- src/fileUtility.test.js
+++ src/fileUtility.test.js
@@ -9,12 +9,23 @@ import * as fs from 'fs';
 
 test("writeToJSONFile creates file", ()=> {
    const path = "./fileCreated.json";
-   const data = "test data";
+   const data = "5";
    const promise = writeToJSONFile(path, data)
-   assert(typeof promise === "object" && typeof promise.then === "function");
-   promise.then((r) => {
-      assert(fs.existsSync(path))
-      fs.unlinkSync(path)
-      assert(!fs.existsSync(path))
+   //assert(typeof promise === "object" && typeof promise.then === "function");
+   return promise.then((r) => {
+      assert(fs.existsSync(path));
    });
 })
+
+test("readFromJSONFile correctly reads file", ()=> {
+   const path = "./fileCreated.json";
+   const promise = readFromJSONFile(path)
+   assert(typeof promise === "object" && typeof promise.then === "function");
+   return promise.then((r) => {
+      assert(typeof r === "string")
+      assert(r==="5")
+      fs.unlinkSync(path)
+      assert(!fs.existsSync(path));
+   });
+})
+
