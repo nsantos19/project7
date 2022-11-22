4f774b7e28abdbce55f38232d6a1eeb6287d5f62
gwarren3210 <gavrielewarren@gmail.com>
2022-11-22 13:35:07 -0500

    cleaned code 3rd commit?

--- src/fetchCurrentWeather.js
+++ src/fetchCurrentWeather.js
@@ -8,15 +8,10 @@ export function fetchCurrentWeather(longitude, latitude) {
   searchURL.searchParams.append("hourly", "temperature_2m")
   searchURL.searchParams.append("temperature_unit", "fahrenheit")
   return fetch(searchURL)
-  .then(response => response.json()) // parse the result to a json
+  .then(response => response.json())
   .then(
     json => {
-      //console.log(json.hourly.time)
-      let obj = {};
-      obj.time = json.hourly.time;
-      obj.temperature_2m = json.hourly.temperature_2m;
-      console.log(obj)
-      return obj
+      return {time: json.hourly.time, temperature_2m: json.hourly.temperature_2m}
     }
   )
   .catch(err => console.log("Unable to get weather with: " + err));
